ac0e35320d3bad4234aaeae784de15e811d2dad8
George Mathew <george.meg91@gmail.com>
2016-03-30 23:23:08 -0400

    Project Setup

---
+++ R Code/causality_project.R
@@ -0,0 +1,13 @@
+# Load the libraries 
+
+# Read the input data 
+
+# Build a VAR model 
+
+# Extract the residuals from the VAR model 
+
+# Check for stationarity using the Augmented Dickey-Fuller test 
+
+# Check whether the variables follow a Gaussian distribution  
+
+# Write the residuals to a csv file to build causal graphs using Tetrad software
